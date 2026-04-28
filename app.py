b2604b70203dbcf84ee1a1e4c10bf09e8f3dc63d
Joshua Richardson <jric0040@student.monash.edu>
2026-04-28 16:13:07 +1000

    Added runnable message with history, and history aware retriever for both retrival and chat context

--- app.py
+++ app.py
@@ -1,66 +1,105 @@
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings, ChatOllama
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+)
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables import RunnableWithMessageHistory
+from langchain_classic.chains.history_aware_retriever import create_history_aware_retriever
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
 from pydantic import BaseModel  # data validation library
 from chainlit.utils import mount_chainlit
+from operator import itemgetter
 
+# LLM
+llm = ChatOllama(model="qwen3.5:latest", reasoning=False)
 
-# Load existing Chroma DB
+# Prompt
+prompt_final = ChatPromptTemplate.from_messages(
+    [
+        ("system", 
+            "You are a personal assistant to Josh and Brina.  "
+            "You exist to help them answer questions about their lives.  "
+            "Use the provided context to help you answer their question: {context}"
+        ),
+        ("placeholder", "{chat_history}"), # expands a list of message objects e.g. [HumanMessage("what is 1+1"), AIMessage("1+1 = 2")]
+        ("human", "{input}"),
+    ]
+)
+
+# Create retriever from existing Chroma DB
 embed_function = OllamaEmbeddings(model="nomic-embed-text")
 vector_db = Chroma(
     collection_name="home_llm",
     embedding_function=embed_function,
     persist_directory="chroma_db",
 )
-
-# Create retriever
-retriever = vector_db.as_retriever(search_kwargs={"k": 3})
-
-# Prompt
-prompt = ChatPromptTemplate.from_template(
-    """
-You are a helpful assistant. Use the context below to answer the question.
-If you don't know, just say you don't know.
-
-Context: {context}
-
-Question: {question}
-"""
+retriever = vector_db.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"k": 3, "score_threshold": 0.5}
 )
 
-# LLM
-llm = ChatOllama(model="qwen3.5:latest", reasoning=False)
+
+# Context Prompting using the Retriever
+contextualize_prompt = ChatPromptTemplate.from_messages([
+    ("system", 
+        "Given the chat history and the latest user question, "
+        "rephrase the question as a standalone question that can be understood "
+        "without the chat history. Do NOT answer it, just rephrase it."
+    ),
+    ("placeholder", "{chat_history}"),
+    ("human", "{input}"),
+])
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_prompt
+)
+
+# History store
+store = {}
+def get_session_history(session_id: str):
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+# Chain
+chain = (
+    {
+        "context": history_aware_retriever, # context is then populated via create_history_aware_retriever
+        "input": itemgetter("input"),
+        "chat_history": itemgetter("chat_history")
+    } 
+    | prompt_final 
+    | llm
+)
+chain_with_history = RunnableWithMessageHistory(
+    chain, # pass input and chat_history into chain
+    get_session_history, # grabs configurable:session_id from the call, passes it into the session_id param in get_session (runs first)
+    input_messages_key="input",
+    history_messages_key="chat_history", #inject history from get_session_history into this key, 
+)
+
+# Mount UI to fast api app
+app = FastAPI()
+mount_chainlit(app=app, target="cl_app.py", path="/chainlit")
+
+
 
 
 # Pypandic query class
-class QueryRequest(BaseModel):
-    query: str
+#class QueryRequest(BaseModel):
+#    query: str
+
+# @app.post("/query")
+# async def query_model(request: QueryRequest):
+#    response = await chain.ainvoke(request.query)
+#    return {"answer": response.content}
 
 
-# Ask
-app = FastAPI()
-
-# Chain
-chain = {"context": retriever, "question": RunnablePassthrough()} | prompt | llm
+# async def stream_generator(question: str):
+#    async for chunk in chain.astream(question):
+#        yield chunk.content
 
 
-@app.post("/query")
-async def query_model(request: QueryRequest):
-    response = await chain.ainvoke(request.query)
-    return {"answer": response.content}
-
-
-async def stream_generator(question: str):
-    async for chunk in chain.astream(question):
-        yield chunk.content
-
-
-@app.post("/stream")
-async def stream_query(request: QueryRequest):
-    return StreamingResponse(stream_generator(request.query), media_type="text/plain")
-
-
-mount_chainlit(app=app, target="cl_app.py", path="/chainlit")
+# @app.post("/stream")
+# async def stream_query(request: QueryRequest):
+#    return StreamingResponse(stream_generator(request.query), media_type="text/plain")
--- cl_app.py
+++ cl_app.py
@@ -1,16 +1,20 @@
 import chainlit as cl
 from app import chain
+from app import chain_with_history
+from app import store
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
     msg = cl.Message(content="")
 
-    # chain.astream(message.content) starts the pipeline
-    # async for chunk in... means whenever it returns a chunk, do something
-    # msg.stream_token (appends the token to the UI in real time)
-    # msg.update (stream is finished, finalises the message)
-
-    async for chunk in chain.astream(message.content):
-        await msg.stream_token(chunk.content)
+    async for chunk in chain_with_history.astream(
+        {"input": message.content},
+        config={"configurable": {"session_id": cl.user_session.get("id")}},
+    ):
+        if hasattr(chunk, "content") and chunk.content:
+            await msg.stream_token(chunk.content)
     await msg.update()
+
+    print("SESSION ID:", cl.user_session.get("id"))
+    print("STORE:", store)
